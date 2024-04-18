a6396b2e428a601f562df81e27f8427f3825119e
rczb <rc-zb@outlook.com>
2024-04-18 19:34:13 +0800

    Dev env updated.

--- flake.nix
+++ flake.nix
@@ -9,21 +9,35 @@
     pkgs = flakeInputs.nixpkgs.legacyPackages.${system};
   in {
     devShells.${system} = {
-      python3 = pkgs.mkShell {
+      nixpkgs = pkgs.mkShell {
         name = "brain-diffuser";
         packages = with pkgs; [
           (python3.withPackages (ps: with ps; [
+            easydict
+            einops
             h5py
             imageio
+            matplotlib
+            mpi4py
             nibabel
             numpy
+            pillow
+            pyyaml
             scikit-image
             scipy
+            tensorboard
+            tensorboardx
             torch
+            torchmetrics
+            torchvision
+            tqdm
+            transformers
+            omegaconf
+            opencv4
           ]))
         ];
       };
-      default = flakeOutputs.devShells.${system}.python3;
+      default = flakeOutputs.devShells.${system}.nixpkgs;
     };
   };
 }
